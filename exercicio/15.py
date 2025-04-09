782e946e9ba96e647db4dcd54e53f44bedd94884
LukazAllan <allanlucas.assisribeiro@gmail.com>
2025-04-09 20:44:32 -0300

    aula09 - ex15

---
+++ exercicio/15.py
@@ -0,0 +1,6 @@
+fator1 = int(input("Digite um número inteiro meu nobre: "))
+    print(f'Tabuada do {fator1}')
+    for fator2 in range(1, 11, 1):
+        print(f'{fator1} x {fator2} == {fator1 * fator2}')
+    print('-='*30)
+ 
