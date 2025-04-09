5b23d9136a52a20826e3f16a33cde87fabef729f
LukazAllan <allanlucas.assisribeiro@gmail.com>
2025-04-09 20:53:31 -0300

    aula09 - ex15

--- exercicio/15.py
+++ exercicio/15.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 for fator1 in range(1, 11, 1):
+=======
+fator1 = int(input("Digite um número inteiro meu nobre: "))
+>>>>>>> 782e946 (aula09 - ex15)
     print(f'Tabuada do {fator1}')
     for fator2 in range(1, 11, 1):
         print(f'{fator1} x {fator2} == {fator1 * fator2}')
