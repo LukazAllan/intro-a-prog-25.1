1f5e26215e8ae20515a3f5296e81ff4858e0641d
LukazAllan <allanlucas.assisribeiro@gmail.com>
2025-04-09 21:10:58 -0300

    aula09 - ex16

---
+++ exercicio/16.py
@@ -0,0 +1,9 @@
+amigos = ["Frankk", "Felipe", "Fernando", "Mitchell"]
+print("(PT-BR) Estes são meus amigos:")
+print("(EN-US) These are my friends:")
+print("(JA-JA) 友達は")
+print("(FR-FR) C'est mon amis:")
+print("(ES-AR) Estos son mis amigos:")
+
+for amigo in amigos:
+    print(f"\tO {amigo}")
